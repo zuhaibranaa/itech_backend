f4551e0de7e83c2e8c689ef2266cb59ed0e4a3de
zuhaib <zuhaibranaa@gmail.com>
2022-06-27 10:48:52 +0500

    updated contents

--- RouterOS/models.py
+++ RouterOS/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ProfileGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class NAS(models.Model):
+    name = models.CharField(max_length=100)
